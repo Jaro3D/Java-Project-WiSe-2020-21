f0cab364459204dcaaeb92b537d807f9b3582ae7
Marlon <marlon.ringel@stud.fra-uas.de>
2021-01-19 15:21:41 +0100

    Datenbank Klasse für bessere benutzbarkeit kommentiert. Alle static methoden aus der e-mail klasse in die NotificationService klasse verschoben und Code entsprechend angepasst.

--- src/notifikationservice/Datenbank.java
+++ src/notifikationservice/Datenbank.java
@@ -12,27 +12,40 @@ import java.util.Date;
  * This class is only to summulate an database for testing purposes.
  */
 public class Datenbank {
+    /*Diese 4 Attribute müssen so wie sie sind in die Datenbank.
+    außerdem muss man sie einzeln laden können 
+    für den Constuctor der E-mail Klasse.
+    */
     public static String AddressSender = "onTime.notification@gmail.com";
     public static String Password = "2Szz59bGHXBK3Lw";
     public static String Host = "smtp.gmail.com";
     public static String Port = "465";
-    public static String NameSender = "onTime notification";
-    public static String subject = "Youre Appointment is now";
-    public static String emailText = "You recived an Email on Time\nCongrats";
-    public static String AddressReceiver = "marlon.ringel@gmx.de";
+    
+    
+    
+    /*Diese 2 Attribute sind nur für die ausführung der fake Datenbank Klasse relevant*/
     public static boolean createNew = true;
     public static Notification[] aktive;
     
+    
+    /*Diese methode simmulert das laden eines notification arrays aus der Datenbank.
+    Hier kann man zu test zwecken die empfänger email adressen anpassen.*/
     public static Notification[] loadNotification()
     {
         Notification[] notifications;
         if(createNew)
         {
-            Date now = Email.getNow();
-            Date end = Email.getNow();
+            Date now = NotifikationService.getNow();
+            Date end = NotifikationService.getNow();
             end.setDate(end.getDate()+15);
             
             Date[] dates = new Date[3];
+            
+            
+            /*Hier kann man das sende datum der notification festlegen.
+            Satndartmäßig wird das aktuelle datum geladen. Es werden drei notifications erstellt,
+            die erste wird sofort versendet, die zweite eine Minute nach programm aufruf, 
+            die letzte nach dem programm aufruf.*/
             dates[0] = new Date(now.getYear(),now.getMonth(),now.getDate(),now.getHours(),now.getMinutes());
             dates[1] = new Date(now.getYear(),now.getMonth(),now.getDate(),now.getHours(),now.getMinutes()+1);
             dates[2] = new Date(now.getYear(),now.getMonth(),now.getDate(),now.getHours(),now.getMinutes()+2);
@@ -47,9 +60,11 @@ public class Datenbank {
             
             notifications = new Notification[3];
             
-            notifications[0] = new Notification("User1", "marlon.ringel@stud.fra-uas.de", "none", "Email-Service test 1", members, address,0,dates[0],end,0);
-            notifications[1] = new Notification("User2", "marlon.ringel@stud.fra-uas.de", "none", "Email-Service test 2", members, address,0,dates[1],end,1);
-            notifications[2] = new Notification("User3", "marlon.ringel@stud.fra-uas.de", "none", "Email-Service test 3", members, address,0,dates[2],end,2);
+            /*Hier(Da wo "give-me-cookies@web.de" steht) kann man die emfänger email adresse festlegen. Dies muss für jede notification eintzeln gemacht werden.
+            An stelle des Strings "none" können CC adressen angeben werden. Sind es mehrere müssen diese durch ein komma getrennt werden.*/            
+            notifications[0] = new Notification("User1", "give-me-cookies@web.de", "none", "Email-Service test 1", members, address,0,dates[0],end,0);
+            notifications[1] = new Notification("User2", "give-me-cookies@web.de", "none", "Email-Service test 2", members, address,0,dates[1],end,1);
+            notifications[2] = new Notification("User3", "give-me-cookies@web.de", "none", "Email-Service test 3", members, address,0,dates[2],end,2);
             
             
             createNew = false;
